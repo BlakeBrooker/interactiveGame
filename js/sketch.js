b687421442be91a9320a5f2765ff641b5d74e04a
BlakeBrooker <116875628+BlakeBrooker@users.noreply.github.com>
2022-11-15 12:08:40 +0000

    Add files via upload

--- js/sketch.js
+++ js/sketch.js
@@ -1,11 +1,49 @@
+var widthRect= 50 ;
+var heightRect= 60 ;
 function setup() {
     var canvas = createCanvas(800, 800);
     canvas.parent("p5container");
+    background(220);
 }
 
 function draw() {
-    background(220);
+    
 
-    fill(255, 255, 255);
-    ellipse(400, 400, 200);
+}
+
+function mousePressed(){
+
+
+if(mouseButton == LEFT)
+{fill(255, 255, 255);
+    rectMode(CENTER);
+    rect(mouseX,mouseY, widthRect, heightRect);
+}
+if(mouseButton ==  RIGHT) 
+{fill(random(0,255),random(0,255),random(0,255));;
+    ellipseMode(CENTER);
+    ellipse(mouseX,mouseY, widthRect, heightRect);
+}
+
+
+}  
+
+function keyPressed(){
+    if(keyCode == ENTER){
+        background(0);
+    }
+    if(keyCode == UP_ARROW){
+        fill(random(0,255),random(0,255),random(0,255));
+        triangle(mouseX, mouseY, mouseX+50, mouseY, mouseX+25, mouseY-50);
+    }
+
+        if(keyCode == LEFT_ARROW){
+            fill(random(0,255),random(0,255),random(0,255));
+            triangle(mouseX, mouseY, mouseX+50, mouseY, mouseX+50, mouseY-25);
+        }
+
+      if(keyCode == RIGHT_ARROW){
+        fill(random(0,255),random(0,255),random(0,255));
+        square(mouseX,mouseY, 50);
+      }
 }
